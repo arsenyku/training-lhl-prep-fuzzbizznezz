f06beebf213b03ad2558cb800b5e1e755373d0c9
Arsenyk Ustaris <arsenyku@users.noreply.github.com>
2015-07-13 16:41:31 -0700

    Initial Commit

---
+++ fuzzbizznezz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fuzzbizznezz
+//
+//  Created by asu on 2015-07-13.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
